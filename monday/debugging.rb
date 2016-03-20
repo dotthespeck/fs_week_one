045356c44bd9f60e5c7653430d8f22ed8bbaea88
Zoe <doticus@gmail.com>
2016-03-19 21:52:24 -0400

    Added module on debugging

---
+++ monday/debugging.rb
@@ -0,0 +1,28 @@
+#Debugging tools
+
+#One of the best debugging tools that you can use in Ruby is called irb (Interactive Ruby)
+#You can access it from the command line by entering 'irb'
+#You can try out commands in Ruby to see if they work in irb. It's a handy tool
+#Go in to irb and try some of the commands we learned last time, such as 'puts'
+#You can also do math using Ruby in irb. Try some equations
+
+#Another tool for debugging in Ruby is called pry. Pry is a Ruby gem. Gems allow you to expand Ruby's capabilities.
+#Ruby gems are installed automatically in versions of Ruby higher than 1.8.
+#Run the command 'ruby -v' in your terminal to see which version of Ruby you are running
+#After that, you'll need to install pry: enter 'gem install pry' in the command line
+
+#to add a gem to your program, you need to require it
+require 'pry'
+
+#There is a small bug in this program, you might pick it out right away
+#But we are going to use pry to see if we can find it
+
+puts "What is your name?"
+names = gets.chomp
+#This command will pause the program in the command line. Check what is in the variable 'name' and 'names'
+binding.pry
+#When you are done with pry, enter '!!!' to stop it on the line with the binding.pry
+#You can continue running the program if you type 'exit' instead. That will show you the error.
+puts "Hello " + name + ", welcome to Ruby!"
+#Be sure to look at the error message to help fix the problem!
+#Error messages are extremely helpful in Ruby and Rails, so it's good to pay close attention to them!
--- monday/strings1.rb
+++ monday/strings1.rb
@@ -1,6 +1,7 @@
 #You can leave comments in your Ruby code by starting a line with a number sign
+#Feel free to make changes to the code to see what happens! You can always pull down a new copy of the original
 
-#strings in Ruby can be enclosed in single or double quotes
+#strings--lines of text--in Ruby need be enclosed in single or double quotes
 #if you use a contraction, you will need double quotes (the example below has 'What's' as an example)
 #'puts' will print the string to screen: it means 'put string'
 
@@ -8,8 +9,15 @@ puts "What's your name?"
 
 #here we are saving the input to the question in the variable 'name'
 #variables in Ruby are created by simply putting the word you want to use (here 'name'), then making it equal to the value you want
+
+#'gets' takes the input from the command line
+#'chomp' gets rid of an automatic new line that Ruby adds with 'gets'. You can try taking out the 'chomp' to see what happens.
 name = gets.chomp
+
+#the plus signs add the content of the variable to the string
 puts "Hello " + name + ", welcome to learning Ruby!"
+
+#You can run this file from the command line by entering 'ruby strings1.rb'
 
 #EXERCISE
 #add another question to ask. Be sure to print it to the screen with 'puts'
